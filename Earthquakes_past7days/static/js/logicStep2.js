06bea58fd260edd56f8fe4fbe6a473c440c99dc4
mengxiaomeng01 <mengxiao23@gmail.com>
2020-06-25 17:35:00 -0500

    updating Earthquakes_past7days branch.

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -63,8 +63,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
 
-	// We turn each feature into a circleMarker on the map.
-	
+	// We turn each feature into a circleMarker on the map.	
 	pointToLayer: function(feature, latlng) {
 				  console.log(data);
 				  return L.circleMarker(latlng);
